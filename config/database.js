43587c68e0df9340d9a2cf9f08092607d6df18c6
Leo Hulu <leohulu02@gmail.com>
2025-05-01 11:33:45 +0700

    ....

--- config/database.js
+++ config/database.js
@@ -4,7 +4,8 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
-    ssl: false, // Opsional: aktifkan SSL jika diperlukan
+    ssl: true, // Opsional: aktifkan SSL jika diperlukan
+    rejectUnauthorized: false, 
   },
 });
 
